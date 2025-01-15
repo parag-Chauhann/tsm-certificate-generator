6e64577ee974da828e51c8ce451ee226859a14e2
Parag Chauhan <paragchauhan33@gmail.com>
2025-01-15 12:17:43 +0530

    a

--- src/components/WordToPdfConverter.js
+++ src/components/WordToPdfConverter.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./WordToPdfConvertor.css";
+import "./wordToPdfConvertor.css";
 
 const WordToPdfConverter = () => {
   const [files, setFiles] = useState([]);
